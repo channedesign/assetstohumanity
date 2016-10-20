366eeb1e7d3e606f1651489b32b333f7b7c88f74
ChanneDesign <channebertrand@gmail.com>
2016-10-20 12:22:51 -0400

    removing turbolink

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,30 +1,36 @@
 $(document).ready(function() {
-	// Subscriptions
-    $(".enter-email, .angle-right").click(function() {
-        var subscribingAnimation = new TimelineMax()
-                .to($(".borderTopBottom"), 0.5, { height: 0, padding: 0 })
-                .to($(".enter-email, .angle-right"), 0.1, { opacity: 0, display: 'none' }, "-=0.5")
-                .to($(".borderTopBottom"), 0.5, { height: 55, padding: 10 })
-                .to($(".show-on-click-subscribe"), 0.5, { display: "inline-block" });
 
-    });
-    $(".angle-right-submit").click(function(){
-      $('#subscribing').submit();
-      return false;
-    });
-    $("#subscribe-email").keypress(function(event) {
-        if (event.which == 13) {
-          event.preventDefault();
-          $("#subscribing").submit();
-        }
-    });
-    $(document).ajaxStart(function(){
-        $(".uil-spin-css").show();
-        $(".angle-right-submit").hide();
-    });
-   
-    $(document).ajaxStop(function(){
-        $(".uil-spin-css").hide();
-        $(".angle-right-submit").show();
-    });
+	// Preloader
+  $(window).load(function(){
+    TweenMax.to($('.preloader'), 1, { opacity: 0, ease: Power1.easeIn, zIndex: "-9999" });
+  });
+
+	// Subscriptions
+  $(".enter-email, .angle-right").click(function() {
+    var subscribingAnimation = new TimelineMax()
+              .to($(".borderTopBottom"), 0.5, { height: 0, padding: 0 })
+              .to($(".enter-email, .angle-right"), 0.1, { opacity: 0, display: 'none' }, "-=0.5")
+              .to($(".borderTopBottom"), 0.5, { height: 55, padding: 10 })
+              .to($(".show-on-click-subscribe"), 0.5, { display: "inline-block" });
+
+  });
+  $(".angle-right-submit").click(function(){
+    $('#subscribing').submit();
+    return false;
+  });
+  $("#subscribe-email").keypress(function(event) {
+    if (event.which == 13) {
+      event.preventDefault();
+      $("#subscribing").submit();
+    }
+  });
+  $(document).ajaxStart(function(){
+    $(".uil-spin-css").show();
+    $(".angle-right-submit").hide();
+  });
+ 
+  $(document).ajaxStop(function(){
+    $(".uil-spin-css").hide();
+    $(".angle-right-submit").show();
+  });
 });
