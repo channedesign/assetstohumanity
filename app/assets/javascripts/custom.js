c39665a4191bcc7f66c86e934c26fb1ef15857a1
ChanneDesign <channebertrand@gmail.com>
2016-11-01 11:01:50 -0400

    add address

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,9 +1,60 @@
+// Adding method to jQuery
+$.fn.clicktoggle = function(a, b) {
+    return this.each(function() {
+        var clicked = false;
+        $(this).click(function(e) {
+            e.preventDefault;
+            if (clicked) {
+                clicked = false;
+                return b.apply(this, arguments);
+            }
+            clicked = true;
+            return a.apply(this, arguments);
+        });
+    });
+};
 $(document).ready(function() {
+  var $tm = TweenMax;
+  var oneT = 1;
+  var fastT = 0.2;
+  if ($(window).width() > 767) {
+    $("#splash").css("min-height", $(window).height());
+  }
 
 	// Preloader
   $(window).load(function(){
     TweenMax.to($('.preloader'), 1, { opacity: 0, ease: Power1.easeIn, zIndex: "-9999" });
   });
+
+   // Nav show/hide
+  $('.nav-buttons img').clicktoggle(function() {
+    $('.info-button').addClass('blue');
+    $tm.to($('.hide-on-click'), oneT, { opacity: 0 });
+    $tm.to($('.show-on-click'), oneT, { opacity: 1 });
+    $tm.to($('.slide-on-click'), oneT, { opacity: 1, right: 120, zIndex: 99999 });
+  }, function() {
+    $('.contact-button').removeClass('blue');
+    $tm.to($('.hide-on-click'), oneT, { opacity: 1 });
+    $tm.to($('.show-on-click'), oneT, { opacity: 0 });
+    $tm.to($('.slide-on-click'), oneT, { opacity: 0, right: -300, zIndex: 9 });
+    $tm.to($(".show-on-click-info"), oneT, { opacity: 1, delay: 0.5 });
+    $tm.to($(".show-on-click-contact"), oneT, { opacity: 0 });
+  });
+
+   // Nav Menu
+    $('.info-button').click(function() {
+      $(this).addClass('blue');
+      $('.contact-button').removeClass('blue');
+      $tm.to($(".show-on-click-info"), oneT, { opacity: 1, delay: 0.5 });
+      $tm.to($(".show-on-click-contact"), oneT, { opacity: 0 });
+    });
+    
+    $(".contact-button").click(function() {
+      $(this).addClass('blue');
+      $('.info-button').removeClass('blue');
+      $tm.to($(".show-on-click-info"), oneT, { opacity: 0 });
+      $tm.to($(".show-on-click-contact"), oneT, { opacity: 1, delay: 0.5 });
+    })
 
 	// Subscriptions
   $(".enter-email, .angle-right").click(function() {
